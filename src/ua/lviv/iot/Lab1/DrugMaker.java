c3b2736da7b28f6c235a38f0fad41596c082e092
Daniil Oleshchuk <57315978+daniiloleshchuk@users.noreply.github.com>
2020-01-23 09:26:29 +0200

    Formated code

--- src/ua/lviv/iot/Lab1/DrugMaker.java
+++ src/ua/lviv/iot/Lab1/DrugMaker.java
@@ -1,10 +1,10 @@
-package ua.lviv.iot.Lab1;
+package ua.lviv.iot.firstLab;
 
 public class DrugMaker {
     public static void main(String[] args) {
         Drug essentiale = new Drug();
-        Drug karvalol = new Drug(50,"α-bromisovalerianic acid ethyl ester, phenobarbital, peppermint oil",50,"transparent",1);
-        Drug anaferon = new Drug("Anaferon","Use after eating",15,"some active subtance",8,"white",20);
+        Drug karvalol = new Drug(50, "α-bromisovalerianic acid ethyl ester, phenobarbital, peppermint oil", 50, "transparent", 1);
+        Drug anaferon = new Drug("Anaferon", "Use after eating", 15, "some active subtance", 8, "white", 20);
 
         System.out.println("\n");
         System.out.println(essentiale.toString());
@@ -15,15 +15,15 @@ public class DrugMaker {
         System.out.println("\n\n\n");
 
         Drug[] arrayOfDrugs = new Drug[4];
-        int counter=0;
+        int counter = 0;
         do {
-            arrayOfDrugs[counter]= new Drug();
+            arrayOfDrugs[counter] = new Drug();
             counter++;
 
-        } while (counter<4);
+        } while (counter < 4);
 
-        for (Drug i: arrayOfDrugs) {
-            System.out.println(i.toString());
+        for (Drug drug : arrayOfDrugs) {
+            System.out.println(drug.toString());
             System.out.println("\n");
         }
     }
